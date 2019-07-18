9a2fde1d11e7977e21e23bd2873bfc7e5b017800
JonLuca DeCaro <jdecaro@pinterest.com>
2019-07-18 13:24:20 -0700

    better functionality

--- js/background.js
+++ js/background.js
@@ -4,7 +4,7 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.query({}, tabs =>{
     const randID = Math.floor(Math.random() * tabs.length);
-    chrome.tabs.update(tabs[randID].id, {highlighted: true});
+    chrome.tabs.update(tabs[randID].id, { active: true});
   } );
 });
 
